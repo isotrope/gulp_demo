55c84ea8d38eec46ceded278620bbc2f5e22fcc0
Michal Bluma <clients@isotrope.net>
2015-08-26 09:03:22 -0400

    Now with a Screenshots job!

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,9 @@ var concat = require('gulp-concat'),
 	notify = require("gulp-notify"),
 	jshint = require('gulp-jshint'),
 	uglify = require('gulp-uglify'),
-	combineMq = require('gulp-combine-mq');
+	combineMq = require('gulp-combine-mq'),
+	Pageres = require('pageres');
+
 
 
 var vendorsFolder = 'vendors/';
@@ -74,7 +76,17 @@ gulp.task('vendor-scripts', function () {
 		.pipe(notify({message: 'Scripts task complete'}));
 });
 
+gulp.task('screenshots', function () {
+	var pageres = new Pageres({delay: 2})
+		.src('localhost/gulp_demo/', ['480x320', '1024x768', 'iphone 5s'], {crop: true})
+		.src('localhost/gulp_demo/2012/01/07/template-sticky/', ['1280x1024', '1920x1080'])
+		.dest('screenshots');
 
+	pageres.run(function (err) {
+		console.log('done');
+	});
+
+});
 
 // Rerun the task when a file changes
 gulp.task('watch', function () {
